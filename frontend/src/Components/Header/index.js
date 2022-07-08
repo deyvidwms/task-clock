46b3a31e5226a153c648de9e7688cb4d7053d58d
Deyvid William <deyvidwms@gmail.com>
2022-07-08 16:56:59 -0300

    Implementação do menu e correção do botão deste

--- frontend/src/Components/Header/index.js
+++ frontend/src/Components/Header/index.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { FaPlus } from "react-icons/fa";
+import { FaBars, FaPlus, FaTimes } from "react-icons/fa";
 
 import './index.css';
 
 function Header() {
+  const [toggleMenu, setToggleMenu] = useState(true);
+
+  const handleToggleMenu = () => {
+    const element = document.getElementsByClassName("button-menu")[0];
+    const showMenu = element.getAttribute("data-toggle").indexOf("false") !== -1 ? true : false;
+    setToggleMenu(showMenu);
+  }
+
   return (
     <header className='header'>
+
       <h1 className='title'>TaskClock</h1>
-      <div className="header--options">
-        <div className="options--add-task">
-          <FaPlus />
+      
+      <div className="header--button-menu">
+        <div className="button-menu" data-toggle={toggleMenu} onClick={ () => handleToggleMenu() } >
+          { toggleMenu ? (<FaBars />) : (<FaTimes />) }
         </div>
       </div>
+
+      <div className={toggleMenu ? "header--menu" : "header--menu active"}>
+        <div className="options--add-task">
+          <p> <FaPlus /> Nova tarefa </p>
+        </div>
+      </div>
+
     </header>
   );
 }
